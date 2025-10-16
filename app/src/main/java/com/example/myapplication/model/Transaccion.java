e577ed186015245be54fa576e43005670fa6dece
Vicente <v.cofre14@alimnos.santotomas>
2025-10-15 23:40:29 -0300

    Añadir modelo de datos para TransaccionSe crea la clase `Transaccion` para representar operaciones financieras genéricas (ingresos o egresos).
    Almacena un ID, fecha, nombre, precio y tipo de transacción. Los objetos son inmutables después de su creación para garantizar la integridad de los datos.

--- app/src/main/java/com/example/myapplication/model/Transaccion.java
+++ app/src/main/java/com/example/myapplication/model/Transaccion.java
@@ -26,3 +26,5 @@ import java.util.Date;public class Transaccion {
         return "Transacción (" + tipo + "): " + nombre + " - $" + String.format("%.2f", precio);
     }
 }
+//Añadir modelo de datos para TransaccionSe crea la clase `Transaccion` para representar operaciones financieras genéricas (ingresos o egresos).
+//Almacena un ID, fecha, nombre, precio y tipo de transacción. Los objetos son inmutables después de su creación para garantizar la integridad de los datos.
