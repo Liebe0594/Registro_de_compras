fa4f000c67aa456e28fd567907fbd8b433c10044
Vicente <v.cofre14@alimnos.santotomas>
2025-10-15 23:24:27 -0300

    Producto

--- app/src/main/java/com/example/myapplication/Producto.java
+++ app/src/main/java/com/example/myapplication/Producto.java
@@ -49,6 +49,7 @@ public class Producto {
     }
 
     // Método para calcular el precio total del producto
+    // prueba cambio
     public double getPrecioTotal() {
         double precioConDescuento = precioUnitario * (1 - (descuento / 100.0));
         return precioConDescuento * cantidad;
